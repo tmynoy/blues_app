a8d40b6adac9ca8971889d309a8b530e078b2879
tmynoy <a6.n_h.x@icloud.com>
2020-11-23 17:05:43 +0900

    コメント機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'teams#index'
   resources :members, only: [:index, :show] do
-    resources :comments, only: [:show]
+    resources :comments, only: [:create]
   end
 end
